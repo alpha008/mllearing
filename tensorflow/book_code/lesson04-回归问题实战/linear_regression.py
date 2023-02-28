58ff0dcf8647c5a60d2af3c0324c076a5f31331d
alpha008 <zjxlove2017@163.com>
2023-02-28 23:56:55 +0800

    线性回归

--- tensorflow/book_code/lesson04-回归问题实战/linear_regression.py
+++ tensorflow/book_code/lesson04-回归问题实战/linear_regression.py
@@ -1,10 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as pyplot
 # y = wx + b
 def compute_error_for_line_given_points(b, w, points):
-    totalError = 0
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
+    totalError = 0  
+    for i in range(0, len(points)): 
+        x = points[i, 0]  #行向量
+        y = points[i, 1]  #列向量
+        if(0==i):
+            print("Starting gradient descent at x = {0}, y = {1}".format(x, y))
         # computer mean-squared-error
         totalError += (y - (w * x + b)) ** 2
     # average loss for each point
@@ -33,23 +36,24 @@ def gradient_descent_runner(points, starting_b, starting_w, learning_rate, num_i
     for i in range(num_iterations):
         b, w = step_gradient(b, w, np.array(points), learning_rate)
     return [b, w]
-
 def run():
-    points = np.genfromtxt("data.csv", delimiter=",")
-    learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_w = 0 # initial slope guess
-    num_iterations = 1000
-    print("Starting gradient descent at b = {0}, w = {1}, error = {2}"
-          .format(initial_b, initial_w,
-                  compute_error_for_line_given_points(initial_b, initial_w, points))
-          )
+    points = np.genfromtxt("data.csv", delimiter=",") #读取数据以逗号分隔数据
+    #print(points)
+    learning_rate = 0.0001 #学习率
+    initial_b = 0 # 初始化截距
+    initial_w = 0 # 初始化斜率
+    num_iterations = 1000  # 迭代次数
+    print("Starting gradient descent at b = {0}, w = {1}, error = {2}".format(initial_b, initial_w,
+                  compute_error_for_line_given_points(initial_b, initial_w, points)))
     print("Running...")
     [b, w] = gradient_descent_runner(points, initial_b, initial_w, learning_rate, num_iterations)
-    print("After {0} iterations b = {1}, w = {2}, error = {3}".
-          format(num_iterations, b, w,
-                 compute_error_for_line_given_points(b, w, points))
-          )
-
+    print("After {0} iterations b = {1}, w = {2}, error = {3}".format(num_iterations, b, w,
+                 compute_error_for_line_given_points(b, w, points)))
+    x = np.linspace(20, 80 , 5)
+    y = w * x + b
+    pyplot.plot(x, y)
+    pyplot.scatter(points[:,0], points[:,1])
+    pyplot.show()
 if __name__ == '__main__':
     run()
+#https://blog.csdn.net/m0_63062182/article/details/124011947
